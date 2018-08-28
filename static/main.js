a0469ba019a17c7dd8e966bffa04df14e3246c6c
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-08-28 16:12:06 +0430

    All page except toturial and news are not ready

--- static/main.js
+++ static/main.js
@@ -42,13 +42,39 @@ $('#owl-two').owlCarousel({
   }
 });
 
+$('#owl-three').owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  smartSpeed: 900,
+  navText: ["<i class='fa fa-chevron-right'></i>", "<i class='fa fa-chevron-left'></i>"],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
 //open-searchbox
 
 $(document).ready(function () {
   $('.open-searchbox').click(function (e) {
+    e.preventDefault();
     $("nav > section").addClass("top-search-open");
+    $("body > .page-address").addClass("page-address-down");
   });
   $('.close-search').click(function (e) {
+    e.preventDefault();
     $("nav > section").removeClass("top-search-open");
+    $("body > .page-address").removeClass("page-address-down");
   });
 });
